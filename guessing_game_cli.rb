5a79d018d5a22af09c0acc432d7913f90fcf8a3c
Andrew Williams <1williamsaj@gmail.com>
2019-07-28 14:55:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -29,7 +29,7 @@ prompt_user
 user_input = user_number
 computer_input = comp_number
 
-if user_input.to_s == "exit"
+if user_input == "exit".to_i
   exit_time
 elsif user_input == computer_input
   you_win
