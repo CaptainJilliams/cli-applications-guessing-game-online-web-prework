fa23b42a8c31c4023d2006f77607166c9d46bb7e
Andrew Williams <1williamsaj@gmail.com>
2019-07-28 14:09:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,14 @@
+def comp_number
+  rand(6)+1
+end
+
+def prompt_user
+  puts "Guess a number between 1 and 6."
+end
+
+
+#------------------------------------------#
+
 def run_guessing_game
-  
+
 end
